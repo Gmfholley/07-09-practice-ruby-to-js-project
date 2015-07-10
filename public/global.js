949d20d85d9aac2fa542714cbd19865475ef767d
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-10 14:54:40 -0500

    Adds javascript to get current record and run ajax on next button

--- public/global.js
+++ public/global.js
@@ -31,9 +31,36 @@ function display_this_one_and_hide_current() {
   toggle_class_name("selected", this);
 }
 
+// the id of the container element is the id
+//
+// returns an Integer of the id
+function get_current_id() {
+  return get_first_of_this_class("container").id
+}
+
+
+//
+
+function next_record(){
+  var next_record = new XMLHttpRequest(); 
+  console.log(("/product/next/" + get_current_id()));
+  next_record.open("get",("/product/next/" + get_current_id()));
+  next_record.addEventListener("load", function(){
+    console.log(this.response);
+  });
+  next_record.send();
+}
+
+function previous_record() {
+  
+}
+
+
 window.onload = function(){
   var all = document.getElementsByClassName("tab");
   for (i = 0; i < all.length; i ++){
     all[i].addEventListener("click", display_this_one_and_hide_current);
   }
+  document.getElementById("next").addEventListener("click", next_record)
+  
 }
