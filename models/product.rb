9227583846a860c81950303230177f7528a5cd0e
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-10 12:44:33 -0500

    Adds valid? and @errors attributes to the model

--- models/product.rb
+++ models/product.rb
@@ -3,13 +3,19 @@
 class Product
   include DatabaseConnector
   
+  attr_reader :id, :general_info, :technical_specs, :where_to_buy, :errors
+  
   def initialize(args={})
     @id = args["id"] || args[:id]
     @general_info = args["general_info"] || args[:general_info]
     @technical_specs = args["technical_specs"] || args[:technical_specs]
     @where_to_buy = args["where_to_buy"] || args[:where_to_buy]
+    @errors = []
   end
   
-  
+  def valid?
+    validate_field_types
+    @errors.length == 0
+  end
 
 end
